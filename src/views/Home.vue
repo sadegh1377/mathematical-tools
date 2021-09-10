d89bfa5cea93996a4e61c78f74ca2023c68e55f7
bagher2901 <bagher.m1377@gmail.com>
2021-09-10 15:33:07 +0430

    2901

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -102,7 +102,7 @@
                                     accordion body. Nothing
                                     more exciting happening here in terms of content, but just filling up the space to
                                     make it look, at
-                                    least at first glance, a bit more representative of how this would
+                                    least at fifffffffrst glance, a bit more representative of how this would
                                     look in a real-world application. Placeholder content for this accordion, which is
                                     intended to
                                     demonstrate the <code>.accordion-flush</code> class. This is the third item's
